fa6f5da369f2b99a1fa2d3b5942f31f72ac0eaa5
Pravinsavaliya <pravinsavaliya801@gmail.com>
2024-03-05 22:53:26 +0530

    cal

---
+++ calculator/coustom.js
@@ -0,0 +1,22 @@
+let tr='';
+const addData = (i)=>{
+    tr += i;
+    document.getElementById("no").value = tr;
+}
+function deleteLastCharacter() {
+    let currentValue = document.getElementById("no").value;
+    document.getElementById("no").value = currentValue.slice(0, -1);
+}
+
+
+function getAns(){
+    let data = document.getElementById("no").value;
+    let ans = eval(data);
+    document.getElementById("no").value = ans;
+    tr = '';
+}
+
+function clearAll(){
+    document.getElementById("no").value = 0;
+    tr = '';
+}
